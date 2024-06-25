90d7a492397e39c86948f08608f6abbb3116211e
Martin <zerisawa33@gmail.com>
2024-06-25 00:59:19 -0500

    chore: Refactor CSS styles for headings, buttons, and inputs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//mobile nav
+// mobile nav
 const mobileNav = document.querySelector(".mnav");
 const closeBtn = document.querySelector(".mnav__close-btn");
 const closeBtnIcn = document.querySelector(".mnav__close-btn-icon");
@@ -9,5 +9,17 @@ const arrowLeftClass = "ri-arrow-left-s-line";
 const arrowRightClass = "ri-arrow-right-s-line";
 
 closeBtn.addEventListener("click", () => {
-  console.log("btn clicked!!");
+  if (mobileNav.classList.contains(navClosedClass)) {
+    console.log("Navigation is closed, opening now."); // Log para depuración
+    mobileNav.classList.remove(navClosedClass);
+    mobileNav.classList.add(navOpenedClass);
+  } else {
+    console.log("Navigation is open, closing now."); // Log para depuración
+    mobileNav.classList.remove(navOpenedClass);
+    mobileNav.classList.add(navClosedClass);
+  }
+
+  // Alternar los iconos
+  closeBtnIcn.classList.toggle(arrowLeftClass);
+  closeBtnIcn.classList.toggle(arrowRightClass);
 });
